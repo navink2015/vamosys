88f054f423341d95be38f5047a20a29516a1c094
unknown <navink2015@gmail.com>
2020-06-16 21:55:01 +0530

    laravel

---
+++ resources/views/lesson1.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link type="text/css" rel="stylesheet" href="/css/app.css"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div id="example">
+        <script src="/js/app.js"></script>
+    </div>
+</body>
+</html>
